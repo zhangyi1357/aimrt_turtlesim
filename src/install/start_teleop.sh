f33d7f65d28c47cfc93b9028a158e858495b39d0
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2024-11-02 01:11:07 +0800

    chore: rename teleop configuration and update parameters
    Adjust the teleop configuration file from .cfg to .yaml for better compatibility, while modifying the listen port and refining backend options to improve communication and functionality.

--- src/install/start_teleop.sh
+++ src/install/start_teleop.sh
@@ -2,4 +2,4 @@
 
 export LD_LIBRARY_PATH=./install/lib:./install/bin:$LD_LIBRARY_PATH
 
-./install/bin/aimrt_main --cfg_file_path=./cfg/teleop.cfg
+./install/bin/aimrt_main --cfg_file_path=./cfg/teleop_cfg.yaml
