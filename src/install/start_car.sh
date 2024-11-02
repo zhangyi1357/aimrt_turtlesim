5a11a3a9cb7d00be8e7b8e424d68cd9ee9e16530
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2024-11-02 16:58:38 +0800

    chore: simplify script execution paths
    Update script commands to remove the unnecessary 'install/bin' prefix, streamlining the execution of the aimrt_main binary. This enhances clarity and maintainability of the scripts.

--- src/install/start_car.sh
+++ src/install/start_car.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./install/bin/aimrt_main --cfg_file_path=./cfg/car_cfg.yaml
+./aimrt_main --cfg_file_path=./cfg/car_cfg.yaml
--- src/install/start_teleop.sh
+++ src/install/start_teleop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./install/bin/aimrt_main --cfg_file_path=./cfg/teleop_cfg.yaml
+./aimrt_main --cfg_file_path=./cfg/teleop_cfg.yaml
